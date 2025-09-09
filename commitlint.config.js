5577d59921679c097f8d61df0c6c25f748cc58e3
Favour Max-Oti <maxotif@gmail.com>
2025-09-09 14:30:31 +0100

    chore: increase header max length in commitlint configuration from 200 to 250

--- commitlint.config.js
+++ commitlint.config.js
@@ -24,7 +24,7 @@ module.exports = {
     "subject-case": [2, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
     "subject-empty": [2, "never"],
     "subject-full-stop": [2, "never", "."],
-    "header-max-length": [2, "always", 200],
+    "header-max-length": [2, "always", 250],
     "body-leading-blank": [1, "always"],
     "footer-leading-blank": [1, "always"],
   },
